8fe93c34f9b598e5348310f536f1d727fce10184
Eloualid <eloualidgd@hotmail.com>
2019-09-06 17:01:57 +0200

    upp

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+document.getElementById("user-score").innerHTML = 0;
+document.getElementById("comp-score").innerHTML = 0;
+
 function play(obj) {
     var object = ["Scissors", "Rock", "Paper"];
 
@@ -25,9 +28,9 @@ function play(obj) {
 
 
     var enemy = Math.floor(Math.random() * 3);
-    console.log(object[obj]);
+ /*   console.log(object[obj]);
     console.log(object[enemy]);
-    console.log(play[obj][enemy]);
+    console.log(play[obj][enemy]); */
     document.getElementById("Player").innerHTML = object[obj];
     document.getElementById("Enemy").innerHTML = object[enemy];
     document.getElementById("Result").innerHTML = play[obj][enemy];
@@ -35,10 +38,22 @@ function play(obj) {
     //document.getElementById("Computer").innerHTML = counter_pc;
 
 
+    var userscore = document.getElementById("user-score").innerHTML ;
+    var compscore = document.getElementById("comp-score").innerHTML ;
+    var result = document.getElementById("Result");
 
-    var res = document.getElementById("Result");
-    if (.res){
-        res =
 
+
+    if(result.innerHTML == "You have lost !!"){
+        compscore++ ;
     }
+
+    if(result.innerHTML == "You've won !!") {
+        userscore++;
+    }
+
+    console.log(compscore);
+    console.log(userscore);
+
+
 }
